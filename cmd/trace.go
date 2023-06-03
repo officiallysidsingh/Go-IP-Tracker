e3fdd3ba15e6db53a784ee23da15228841020ca0
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-06-03 09:43:48 +0530

    added the response command

--- cmd/trace.go
+++ cmd/trace.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/spf13/cobra"
@@ -18,7 +21,8 @@ var traceCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) > 0 {
 			for _, ip := range args {
-				fmt.Println(ip)
+				fmt.Println("IP Address :- ", ip)
+				showData()
 			}
 		} else {
 			fmt.Println("Please provide the IP Address")
@@ -30,10 +34,45 @@ func init() {
 	rootCmd.AddCommand(traceCmd)
 }
 
-func showData() {}
+type IPAddress struct {
+	IP			string		`json:"ip"`
+	City		string		`json:"city"`
+	Region		string		`json:"region"`
+	Country		string		`json:"country"`
+	Loc			string		`json:"loc"`
+	Postal		string		`json:"postal"`
+	Timezone	string		`json:"timezone"`
+	Org			string		`json:"org"`
+}
 
-func getData() {
+func showData() {
 	url := "http://ipinfo.io/1.1.1.1/geo"
+	resByte := getData(url)
 
-	http.Get(url)
+	var data IPAddress
+
+	err := json.Unmarshal(resByte, &data)
+	if err != nil {
+		log.Println("Unable to Unmarshal the data")
+	}
+	
+	fmt.Println("Data Found :- ")
+	fmt.Println(data)
+}
+
+func getData(url string) []byte {
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		log.Println("Unable to get the response")
+	}
+
+	resByte, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		log.Println("Unable to read the response")
+	}
+
+	return resByte
 }
